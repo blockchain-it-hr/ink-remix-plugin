7d1381cb20e0339062043ca4ffe02ba129eafdbc
dark64 <darem966@gmail.com>
2019-11-25 20:25:41 +0100

    add error safety

--- server/src/services/cargo-contract/manager.js
+++ server/src/services/cargo-contract/manager.js
@@ -20,12 +20,21 @@ class CargoContractManager {
 
     spawnProcess(cmd, options, callback, onclose) {
         const child = spawn(cmd, options);
+        child.on('error', (err) => callback({ type: "error", payload: `${err.toString()}` }));
+
         if (callback) {
             child.stdout.on('data', (data)  => callback({ type: "stdout", payload: `${data}` }));
             child.stderr.on('data', (error) => callback({ type: "stderr", payload: `${error}` }));
         }
+
         if (onclose) {
-            child.on('close', onclose);
+            child.on('close', (exitCode) => {
+                if (exitCode != 0) {
+                    callback({ type: "error", payload: `Child process exited with code ${exitCode}` });
+                    return;
+                }
+                onclose();
+            });
         }
     }
 }
