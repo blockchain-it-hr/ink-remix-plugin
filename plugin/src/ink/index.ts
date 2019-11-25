cd904ee07148dcd94fcab522c31bdbfdecfc9897
Edi Sinovcic <edi.sinovcic@gmail.com>
2019-11-25 15:05:10 +0100

    Removing nginx due to proxy config problem

--- plugin/src/ink/index.ts
+++ plugin/src/ink/index.ts
@@ -1,7 +1,6 @@
 import { MessageHandler } from './types';
 import WebSocketManager from './web-socket-manager';
 import { IProject } from '../types';
-import Logger = require('bunyan');
 import { WS_BASE_URL } from '../constants';
 
 class InkService {
