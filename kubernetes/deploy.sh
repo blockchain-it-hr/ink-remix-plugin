e0fcd03bcd6249b2c2636393527918c7d4cb5797
Edi Sinovcic <edi.sinovcic@gmail.com>
2019-12-11 14:17:44 +0100

    Adding helm chart, still certs to be finished

---
+++ kubernetes/deploy.sh
@@ -0,0 +1,7 @@
+
+cd ink-network
+
+helm install --namespace ink --name ink .
+
+helm install stable/nginx-ingress --namespace ink --name ink-ingress --set controller.replicaCount=2 --set rbac.create=true
+kubectl apply -f ingress.yaml
