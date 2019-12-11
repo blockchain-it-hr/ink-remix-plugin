ce5f4e24d5e1ab16974a75c95e89347062fab27f
Edi Sinovcic <edi.sinovcic@gmail.com>
2019-12-11 19:19:46 +0100

    Updating config

--- kubernetes/ink-network/deploy.sh
+++ kubernetes/ink-network/deploy.sh
@@ -1,6 +1,4 @@
 
-cd ink-network
-
 helm install --namespace ink --name ink .
 
 helm install stable/nginx-ingress --namespace ink --name ink-ingress --set controller.replicaCount=2 --set rbac.create=true
