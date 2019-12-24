d7846188ff71d9c539245dff0dfb175245b1e5af
Edi Sinovcic <edi.sinovcic@gmail.com>
2019-12-24 19:23:30 +0100

    Adding environments to json

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,15 +1,3 @@
 curl -XPOST \
-  [https://api.github.com/repos/blockchain-it-hr/ink-remix-plugin/deployments](https://api.github.com/repos/blockchain-it-hr/ink-remix-plugin/deployments) \
-  -d '{
-  "required_contexts": ["build"],
-  "ref": "feature-kubernetes-ci",
-  "environment": "staging",
-  "description": "Staging",
-  "payload": {
-    "value_files": ["./config/staging.yml", "./config/_common.yml"],
-    "release": "staging-ink",
-    "namespace": "ink",
-    "track": "stable",
-    "values": {"replicaCount": 1,"version": "v1"}
-  }
-}'
+  https://api.github.com/repos/blockchain-it-hr/ink-remix-plugin/deployments \
+  -d "@staging.json"
