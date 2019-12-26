816859288dfdb8c7dad532766655831c2bc39aeb
Jakov Buratovic <kraljipo007@gmail.com>
2019-12-26 13:17:01 +0100

    Tested whole backend flow

--- server/test/test.js
+++ server/test/test.js
@@ -26,11 +26,10 @@ describe('cargo-contract service.js', () => {
     });
 
     describe('callback in calling cargoContractManager.build', () => {
-        before(function () {
-            //cargoContractService.build( {projectId: "123", projectName: "testProject", lib: "lib", cargo: "cargo"}, (result) => {} );
-        });
-        /*it('should be able to find target of built project', function () {
+        before(function (done) {
+            this.timeout(3600000);
             args = JSON.parse(example);
+            const abiPath = "./src/storage/961c80e2-72a9-4bfb-b33e-ccfc36ad302f/debug/target/metadata.json";
 
             cargoContractService.build({
                 projectId: args.projectId,
@@ -38,28 +37,31 @@ describe('cargo-contract service.js', () => {
                 lib: args.lib,
                 cargo: args.cargo
             }, (result) => {
-                /!*projectPath = "./src/storage/123";
-                projectName = "testProject";
-                const wasmPath = "./src/storage/123/testProject/target/testProject.wasm";
-                const abiPath = "./src/storage/123/testProject/target/metadata.json";*!/
-                /!*if(result = ""){
-                    /!*assert.ok(fs.readFileSync(wasmPath).toString('base64'));
-                    assert.ok(fs.readFileSync(abiPath).toString());*!/
-                }*!/
-
+                if (Object.values(result).indexOf('build') > -1) {
+                    done();
+                }
             });
-        });*/
+        });
+
+        it('should be able to find target of built project', function () {
+            const wasmPath = "./src/storage/961c80e2-72a9-4bfb-b33e-ccfc36ad302f/debug/target/debug.wasm";
+            const abiPath = "./src/storage/961c80e2-72a9-4bfb-b33e-ccfc36ad302f/debug/target/metadata.json";
+            assert.ok(fs.readFileSync(wasmPath).toString('base64'));
+            assert.ok(fs.readFileSync(abiPath).toString());
+        });
     });
 });
 
 describe('cargo contract manager.js', function () {
-    before(function () {
+    before(function (done) {
         projectName = "1234";
         workspaceDirectory = "./test/storage";
         projectDirectory = "./test/storage/1234";
         libPath = "./test/storage/1234/lib.rs";
         cargoPath = "./test/storage/1234/Cargo.toml";
-        cargoContractService.cargoContractManager.create(projectName, workspaceDirectory, null, null);
+        cargoContractService.cargoContractManager.create(projectName, workspaceDirectory, () => {
+            done();
+        }, null);
     });
     it('should complete build function without errors', function () {
         expect(function () {
